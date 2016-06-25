e896e91715a61c84d3685b7c6ab35495b6244438
lrgpro <liurgui@gmail.com>
2016-06-25 19:28:57 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+﻿var urls = "";
+$(function(){
+	urls = '{"index1":"http://www.mp4ba.com/search.php?keyword=冰与火之歌","index2":16}';  
+    //testJson=eval(testJson);//错误的转换方式   
+    urls = eval("(" + urls + ")"); 
+	
+	$("img").click(function(){
+		giveUrl(this);
+	});
+});
+
+function giveUrl(ele){
+	alert(urls[ele.id]);
+}
